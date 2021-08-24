d9c2e89cdec22075cb884992af7e40b51b3367d7
Mahmud841 <mahmudsuruj14@gmail.com>
2021-08-24 17:29:39 +0800

    final commit

---
+++ quiz.js
@@ -0,0 +1,34 @@
+/* function min(nums) {
+    return Math.min(nums)
+}; */
+
+function min(nums) {
+    return Math.min(nums)
+}
+// console.log(min([1, 3, 2])) ?
+
+
+const cube = x => x * x * x;
+// console.log(cube(2))
+
+const [a, b] = [1, 2, 3, 4, 45, 5];
+console.log(a + b);
+
+const { x, y, z } = { x: 1, y1: 2, z: 3 };
+// console.log(y);
+
+
+const nums = [1, 2, 3, 4, 5];
+let output = nums.filter(n => n % 2);
+// console.log(output);
+
+
+const friends = ["Moushumi", "Misha", "Manna", "mimi" , "mahiya"];
+
+const quiz = friends.find(friend => friend.length == 5);
+console.log(quiz);
+
+/* const product = {name: ‘Laptop’, model:’Yoga 3’, price:49000, dusk: ‘512SSD’} */
+
+/* const name = “Adam Sandler”; 
+const greeting = ‘Hello _____?______, welcome to the club’; */
